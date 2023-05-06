8ad5c15e2c5b5fbfdb06f3b50c19938b40533d5e
Madalena Machado Rocha <rochamada1997m@gmail.com>
2023-05-06 19:58:43 -0300

    :bug: fix NotesController

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -5,7 +5,7 @@ class NotesController {
         const { title, description, tags, links } = request.body;
         const user_id = request.user.id;
 
-        const note_id = await knex("notes").insert({
+        const [note_id] = await knex("notes").insert({
             title,
             description,
             user_id
